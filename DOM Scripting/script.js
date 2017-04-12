55190d009928d07cdefce32681856bad5fc81804
3A46AA1 <olliecee.mail@gmail.com>
2017-04-11 23:02:01 -0400

    Added functionality for Button Submit and Edit

--- DOM Scripting/script.js
+++ DOM Scripting/script.js
@@ -1,87 +1,130 @@
-const form = document.querySelector('#registrar');
-const input = document.querySelector('input');
-const ul = document.getElementById('invitedList');
+document.addEventListener('DOMContentLoaded', () => {
 
+    const form = document.querySelector('#registrar');
+    const input = document.querySelector('input');
+    const ul = document.getElementById('invitedList');
+    const check = document.querySelector('.check');
 
-form.addEventListener('submit', function(event) {
-    // Prevents form submission
-    event.preventDefault();
-    
-    // Vars
-    const text = input.value;
-    const li = document.createElement('li');
+    // Event: Hide or show invitees
+    check.addEventListener('change', (event) => {
+        // Vars
+        const checkbox = event.target;
+        const listItem = ul.children;
 
-    const span = document.createElement('span');
-    span.textContent = text;
-    li.appendChild(span);
+        // Hides non-confirmed invitees
+        if (checkbox.type == 'checkbox' && checkbox.checked) {
+            for (let i = 0; i < listItem.length; i++) {
+                if (listItem[i].className == 'responded') {
+                    listItem[i].style.display = 'block';
+                }
+                else {
+                    listItem[i].style.display = 'none';
+                }
+            }
+        }
 
-    const label = document.createElement('label');
-    label.textContent = 'Confirmed';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-        // Button Edit
-        const buttonEdit = document.createElement('button');
-        buttonEdit.className = 'edit';
-        buttonEdit.type = 'button';
-        buttonEdit.textContent = 'Edit';
-        // Button Remove
-        const buttonRemove = document.createElement('button');
-        buttonRemove.className = 'remove';
-        buttonRemove.type = 'button';
-        buttonRemove.textContent = 'Remove';
+        // Shows all
+        if (checkbox.type == 'checkbox' && checkbox.checked == false) {
+            for (let i = 0; i < listItem.length; i++) {
+                listItem[i].style.display = 'block';
+            }
+        }
+    });
 
-    // Appending values into vars
-    label.appendChild(checkbox);
-    li.appendChild(label);
-    li.appendChild(buttonEdit);
-    li.appendChild(buttonRemove);
-    ul.appendChild(li);
-    input.value = '';
-});
+    // Event: Form
+    form.addEventListener('submit', (event) => {
+        // Prevents form submission
+        event.preventDefault();
+        
+        // Vars
+        const text = input.value;
+        const li = document.createElement('li');
 
-// Event: List
-ul.addEventListener('change', function(event) {
-    // Vars
-    const checkbox = event.target;
-    const checked = checkbox.checked;
-    const listItem = checkbox.parentNode.parentNode;
+        const span = document.createElement('span');
+        span.textContent = text;
+        li.appendChild(span);
 
-    // Adds class to selected list item
-    if (checked) {
-        listItem.className = 'responded';
-    }
-    else {
-        listItem.className = '';
-    }
-});
+        const label = document.createElement('label');
+        label.textContent = 'Confirmed';
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+            // Button Edit
+            const buttonEdit = document.createElement('button');
+            buttonEdit.className = 'edit';
+            buttonEdit.type = 'button';
+            buttonEdit.textContent = 'Edit';
+            // Button Remove
+            const buttonRemove = document.createElement('button');
+            buttonRemove.className = 'remove';
+            buttonRemove.type = 'button';
+            buttonRemove.textContent = 'Remove';
 
-// Event: Buttons
-ul.addEventListener('click', function(event) {
-    // Vars
-    const button  = event.target
-    const li = event.target.parentNode;
-    const ul = event.target.parentNode.parentNode;
+        // Appending values into vars
+        label.appendChild(checkbox);
+        li.appendChild(label);
+        li.appendChild(buttonEdit);
+        li.appendChild(buttonRemove);
+        ul.appendChild(li);
+        input.value = '';
+    });
 
-    // Button remove
-    if (button.className == 'remove') {
-        ul.removeChild(li);
-    }
-    // Button edit
-    else if (button.className == 'edit') {
-        const span = li.firstElementChild;
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = span.textContent;
+    // Event: ul, List
+    ul.addEventListener('change', (event) => {
+        // Vars
+        const checkbox = event.target;
+        const checked = checkbox.checked;
+        const listItem = checkbox.parentNode.parentNode;
 
-        button.className = 'complete';
-        button.textContent = 'submit';
+        // Adds class to selected list item
+        if (checked) {
+            listItem.className = 'responded';
+        }
+        else {
+            listItem.className = '';
+        }
+    });
 
-        li.insertBefore(input, span);
-        li.removeChild(span);
+    // Event: Buttons
+    ul.addEventListener('click', (event) => {
+        // Vars
+        const button  = event.target
+        const li = event.target.parentNode;
+        const ul = event.target.parentNode.parentNode;
+
+        // Button remove
+        if (button.className == 'remove') {
+            ul.removeChild(li);
+        }
+        // Button edit
+        if (button.className == 'edit') {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+
+            const buttonEdit = document.querySelector('.edit');
+            const buttonSubmit = document.createElement('button');
+            buttonSubmit.textContent = 'submit';
+            buttonSubmit.className = 'complete';
+
+            li.insertBefore(buttonSubmit, buttonEdit);
+            li.insertBefore(input, span);
+            li.removeChild(buttonEdit);
+            li.removeChild(span);
+        }
 
         // Button submit
         if (button.className == 'complete') {
+            const span = document.createElement('span');
+            const input = li.firstElementChild;
+            span.textContent = input.value;
+            
+            button.className = 'edit';
+            button.textContent = 'edit';
 
+            li.insertBefore(span, input);
+            li.removeChild(input);
         }
-    }
+    });
+
 });
